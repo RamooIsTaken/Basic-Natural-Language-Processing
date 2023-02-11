18ad94a393ab945684aec0326dc0860f77aa4cc4
RamooIsTaken <remzi.cakir@hotmail.com>
2023-02-11 23:23:38 +0300

    Yorumlar üzerinden basic NPL kodu

---
+++ naturelLanPro.py
@@ -0,0 +1,37 @@
+import pandas as pd
+import numpy as np
+import re
+import nltk
+
+from nltk.stem.porter import PorterStemmer
+from nltk.corpus import stopwords
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.model_selection import train_test_split
+from sklearn.naive_bayes import GaussianNB
+from sklearn.metrics import confusion_matrix
+
+
+df = pd.read_csv('output.csv')
+nltk.download('stopwords')
+ps = PorterStemmer()
+cv = CountVectorizer(max_features=2000)
+
+derlem = []
+for i in range(df.shape[0]):
+    yorum = re.sub('[^a-zA-Z]',' ',df[' Review'][i])
+    yorum = yorum.lower().split()
+    yorum = [ps.stem(kelime) for kelime in yorum if not kelime in set(stopwords.words('english'))]
+    yorum = ' '.join(yorum)
+    derlem.append(yorum)
+
+X = cv.fit_transform(derlem).toarray()
+y = df.iloc[:,1].values
+
+x_train,x_test,y_train,y_test = train_test_split(X,y,test_size=0.2,random_state=0)
+
+gnb = GaussianNB()
+gnb.fit(x_train,y_train)
+yPred = gnb.predict(x_test)
+
+cm = confusion_matrix(y_test,yPred)
+print(cm)
